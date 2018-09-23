a983845f98f10bb0a85899faeae965f9b5bcf5de
CCHxLi <hanxiangli97@gmail.com>
2018-09-22 23:45:54 -0400

    v1.2
    try to load and change data

--- BIA01/BIA01/Form1.Designer.cs
+++ BIA01/BIA01/Form1.Designer.cs
@@ -32,8 +32,18 @@
             System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
             System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
             this.SuspendLayout();
             // 
             // chart1
@@ -57,22 +67,95 @@
             title1.Name = "Title1";
             this.chart1.Titles.Add(title1);
             // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(682, 476);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 95);
+            this.listBox1.TabIndex = 1;
+            // 
+            // chart2
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart2.Legends.Add(legend2);
+            this.chart2.Location = new System.Drawing.Point(437, 64);
+            this.chart2.Name = "chart2";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart2.Series.Add(series2);
+            this.chart2.Size = new System.Drawing.Size(300, 300);
+            this.chart2.TabIndex = 2;
+            this.chart2.Text = "chart2";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(837, 548);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Transform";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(837, 476);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(75, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(683, 432);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Categories";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(837, 431);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Values";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1461, 688);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.listBox1);
             this.Controls.Add(this.chart1);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- BIA01/BIA01/Form1.cs
+++ BIA01/BIA01/Form1.cs
@@ -13,33 +13,49 @@ namespace BIA01
 {
     public partial class Form1 : Form
     {
+
         public Form1()
         {
             InitializeComponent();
             InitChart();
-            LoadData();            
         }
 
-        private void LoadData()
+        private void Changedata()
         {
-            chart1.Series[0].Points.AddXY("EMEA",50.00);
-            chart1.Series[0].Points.AddXY("Asia",30.00);
-            chart1.Series[0].Points.AddXY("Americas", 20.00);
+            string selectedName = listBox1.GetItemText(listBox1.SelectedItem);
+            int selectedValue = Int32.Parse(textBox1.Text);
+            LoadData(selectedName, selectedValue);
+        }
+
+        private void LoadData(string selectedName, int selectedValue)
+        {
+            string[] categories = { "EMEA", "Asia", "Americas" };
+            if (categories.Contains(selectedName))
+            {
+                chart1.Series[0].Points.AddXY(selectedName, selectedValue);
+            }
+
+
+            chart1.Series[0].Points.AddXY("EMEA", 50);
+            chart1.Series[0].Points.AddXY("Asia", 30);
+            chart1.Series[0].Points.AddXY("Americas", 20);
+
+            chart2.Series[0].Points.AddXY("EMEA", 50);
+            chart2.Series[0].Points.AddXY("Asia", 30);
+            chart2.Series[0].Points.AddXY("Ameriacas", 20);
         }
 
         private void InitChart()
         {
-            chart1.Legends[0].Title = "Display Legends";
+            chart1.Legends[0].Title = "Display Legend";
             chart1.Titles.Add("pie first title");
             chart1.Titles.Add("pie second title");
             chart1.Series[0].ChartType = SeriesChartType.Pie;
 
-        }
+            chart2.Legends[0].Title = "Display Legend";
+            chart2.Titles.Add("line chart title");
+            chart2.Series[0].ChartType = SeriesChartType.Line;
 
-        private void chart1_Click(object sender, EventArgs e)
-        {
-           
         }
-        
     }
 }
