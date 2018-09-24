d233321732296312f01e090f2812fcc6655c41fb
CCHxLi <hanxiangli97@gmail.com>
2018-09-23 21:52:16 -0400

    v1.5
    mostly acchived requirements, but the min/max/avr line are fixed value which can be coded in better way to dynamicly change data
    note: study for data binding

--- BIA01/BIA01/Form1.Designer.cs
+++ BIA01/BIA01/Form1.Designer.cs
@@ -28,66 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea5 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend5 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea6 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend6 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea7 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend7 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series7 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea8 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend8 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series8 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
-            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.dgv1 = new System.Windows.Forms.DataGridView();
             this.btnUpdate = new System.Windows.Forms.Button();
+            this.chart3 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart4 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dgv1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
             this.SuspendLayout();
             // 
             // chart1
             // 
-            chartArea1.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea1);
-            legend1.Name = "Legend1";
-            this.chart1.Legends.Add(legend1);
+            chartArea5.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea5);
+            legend5.Name = "Legend1";
+            this.chart1.Legends.Add(legend5);
             this.chart1.Location = new System.Drawing.Point(12, 12);
             this.chart1.Name = "chart1";
-            series1.ChartArea = "ChartArea1";
-            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
-            series1.Legend = "Legend1";
-            series1.Name = "Series1";
-            this.chart1.Series.Add(series1);
+            series5.ChartArea = "ChartArea1";
+            series5.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
+            series5.Legend = "Legend1";
+            series5.Name = "Series1";
+            this.chart1.Series.Add(series5);
             this.chart1.Size = new System.Drawing.Size(300, 300);
             this.chart1.TabIndex = 0;
             this.chart1.Text = "chart1";
             // 
-            // chart2
-            // 
-            chartArea2.Name = "ChartArea1";
-            this.chart2.ChartAreas.Add(chartArea2);
-            legend2.Name = "Legend1";
-            this.chart2.Legends.Add(legend2);
-            this.chart2.Location = new System.Drawing.Point(333, 12);
-            this.chart2.Name = "chart2";
-            series2.ChartArea = "ChartArea1";
-            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
-            series2.Legend = "Legend1";
-            series2.Name = "Series1";
-            this.chart2.Series.Add(series2);
-            this.chart2.Size = new System.Drawing.Size(300, 300);
-            this.chart2.TabIndex = 1;
-            this.chart2.Text = "chart2";
-            // 
             // dgv1
             // 
             this.dgv1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgv1.Location = new System.Drawing.Point(72, 353);
+            this.dgv1.Location = new System.Drawing.Point(393, 355);
             this.dgv1.Name = "dgv1";
             this.dgv1.Size = new System.Drawing.Size(240, 150);
             this.dgv1.TabIndex = 3;
             // 
             // btnUpdate
             // 
-            this.btnUpdate.Location = new System.Drawing.Point(377, 431);
+            this.btnUpdate.Location = new System.Drawing.Point(673, 413);
             this.btnUpdate.Name = "btnUpdate";
             this.btnUpdate.Size = new System.Drawing.Size(75, 23);
             this.btnUpdate.TabIndex = 4;
@@ -95,20 +88,76 @@
             this.btnUpdate.UseVisualStyleBackColor = true;
             this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
             // 
+            // chart3
+            // 
+            chartArea6.Name = "ChartArea1";
+            this.chart3.ChartAreas.Add(chartArea6);
+            legend6.Name = "Legend1";
+            this.chart3.Legends.Add(legend6);
+            this.chart3.Location = new System.Drawing.Point(653, 12);
+            this.chart3.Name = "chart3";
+            series6.ChartArea = "ChartArea1";
+            series6.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series6.Legend = "Legend1";
+            series6.Name = "Series1";
+            series6.YValuesPerPoint = 6;
+            this.chart3.Series.Add(series6);
+            this.chart3.Size = new System.Drawing.Size(300, 300);
+            this.chart3.TabIndex = 5;
+            this.chart3.Text = "chart3";
+            // 
+            // chart4
+            // 
+            chartArea7.Name = "ChartArea1";
+            this.chart4.ChartAreas.Add(chartArea7);
+            legend7.Name = "Legend1";
+            this.chart4.Legends.Add(legend7);
+            this.chart4.Location = new System.Drawing.Point(970, 12);
+            this.chart4.Name = "chart4";
+            series7.ChartArea = "ChartArea1";
+            series7.Legend = "Legend1";
+            series7.Name = "Series1";
+            series7.XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Double;
+            this.chart4.Series.Add(series7);
+            this.chart4.Size = new System.Drawing.Size(633, 624);
+            this.chart4.TabIndex = 7;
+            this.chart4.Text = "chart4";
+            // 
+            // chart2
+            // 
+            chartArea8.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea8);
+            legend8.Name = "Legend1";
+            this.chart2.Legends.Add(legend8);
+            this.chart2.Location = new System.Drawing.Point(333, 12);
+            this.chart2.Name = "chart2";
+            series8.ChartArea = "ChartArea1";
+            series8.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series8.Legend = "Legend1";
+            series8.Name = "Series1";
+            this.chart2.Series.Add(series8);
+            this.chart2.Size = new System.Drawing.Size(300, 300);
+            this.chart2.TabIndex = 8;
+            this.chart2.Text = "chart2";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1279, 527);
+            this.ClientSize = new System.Drawing.Size(1615, 676);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.chart4);
+            this.Controls.Add(this.chart3);
             this.Controls.Add(this.btnUpdate);
             this.Controls.Add(this.dgv1);
-            this.Controls.Add(this.chart2);
             this.Controls.Add(this.chart1);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dgv1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -116,9 +165,11 @@
         #endregion
 
         private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
-        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
         private System.Windows.Forms.DataGridView dgv1;
         private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart3;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart4;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
     }
 }
 
--- BIA01/BIA01/Form1.cs
+++ BIA01/BIA01/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -27,18 +28,78 @@ namespace BIA01
                 dt.Columns.Add("Month");
                 dt.Columns.Add("Sale");
 
-                dt.Rows.Add("Sping", 20);
-                dt.Rows.Add("Summer", 35);
-                dt.Rows.Add("Autumn", 15);
-                dt.Rows.Add("Winter", 30);
+                //int sum = 0;
 
+                for (int i = 1; i <= 12; i++)
+                {
+                    string mnth = CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(i);
+                    DataRow dr = dt.NewRow();
+                    dr[0] = mnth;
+                    dr[1] = i * 2;
+                    dt.Rows.Add(dr);
+
+                    //sum += (int)dr[1];                    
+                }
+
+                chart1.Series[0].Points.DataBind(dt.DefaultView, "Month", "Sale", null);
+                chart2.Series[0].Points.DataBind(dt.DefaultView, "Month", "Sale", null);
+                chart3.Series[0].Points.DataBind(dt.DefaultView, "Month", "Sale", null);
+                chart4.Series[0].Points.DataBind(dt.DefaultView, "Month", "Sale", null);
+
+
+                //sum = dt.Compute("Sum(Sale)", "");               
+
+                //https://stackoverflow.com/questions/5601752/how-to-sum-columns-in-a-datatable
+                //foreach (DataRow dr in dt.Rows)
+                //{
+                //    foreach (DataColumn dc in dt.Columns)
+                //    {
+                //        sum += (string)dr[dc];
+                //    }
+                //}
+
+                //textBox1.Text = "sum/" + sum;
                 dgv1.DataSource = dt;
 
                 chart1.Series[0].XValueMember = "Month";
                 chart1.Series[0].YValueMembers = "Sale";
+
+                chart2.Series[0].XValueMember = "Month";
+                chart2.Series[0].YValueMembers = "Sale";
+
+                chart3.Series[0].XValueMember = "Month";
+                chart3.Series[0].YValueMembers = "Sale";
+
                 this.chart1.Titles.Add("Pie chart title");
-                chart1.Series[0].ChartType = SeriesChartType.Pie;
-                chart1.Series[0].IsValueShownAsLabel = true;
+                this.chart1.Titles.Add("Sale per month");
+                this.chart2.Titles.Add("Line chart title");
+                this.chart2.Titles.Add("Sale per month");
+
+
+                //Series min = new Series("mininum expectation");
+                //Series max = new Series("maximum expectation");
+                //Series avr = new Series("average expectation");
+
+                Series sale = new Series("sale per month");
+                chart2.Series[0].LegendText = "Sale per month";
+
+                chart3.Series[0] = sale;
+                chart3.Series[0].LegendText = "Sale per month";
+
+                this.chart3.Titles.Add("Control chart title");
+                this.chart3.Titles.Add("Sale per month");
+                this.chart4.Titles.Add("Pareto chart title");
+                this.chart4.Titles.Add("Sale per month");
+
+                chart1.Series[0].IsValueShownAsLabel = true;                 
+                chart2.Series[0].IsValueShownAsLabel = true;
+                chart3.Series[0].IsValueShownAsLabel = true;
+                chart3.Series[0].MarkerStyle = MarkerStyle.Cross;
+                chart4.Series[0].IsValueShownAsLabel = true;
+
+
+                makeParetoChart();
+                AddHorizRepeatingStripLines();                
                 PopulateChart();
             }
             catch (Exception)
@@ -48,20 +109,159 @@ namespace BIA01
 
 
         }
+
+        /// <summary>
+        /// Pareto chart study materal reference: https://myminepapers.wordpress.com/tag/pareto-mschart/
+        /// </summary>
+        private void makeParetoChart()
+        {
+
+            // get name of the ChartAre of the source series
+
+            string strChartArea = chart4.Series[0].ChartArea;
+
+            // ensure that the source series is a column chart type
+
+            chart4.Series[0].ChartType = SeriesChartType.Column;
+            chart4.Series[0].LegendText = "Sale per month";
+            // sort the data in all series by their values in descending order
+
+            chart4.DataManipulator.Sort(PointSortOrder.Descending, chart4.Series[0]);
+
+            // find the total of all points in the source series
+
+            double total = 0.0;
+
+            foreach (DataPoint pt in chart4.Series[0].Points)
+
+                total += pt.YValues[0];
+
+            // set the max value on the primary axis to total
+
+            chart4.ChartAreas[strChartArea].AxisY.Maximum = total;
+
+            // create the destination series and add it to the chart
+            
+            Series destSeries = new Series("Persontage");
+            
+            chart4.Series.Add(destSeries);
+            
+            // ensure that the destination series is either a Line or Spline chart type
+
+            destSeries.ChartType = SeriesChartType.Line;
+
+            destSeries.BorderWidth = 3;
+
+            // assign the series to the same chart area as the column chart is assigned
+
+            destSeries.ChartArea = chart4.Series[0].ChartArea;
+
+            // assign this series to use the secondary axis and set it maximum to be 100%
+
+            destSeries.YAxisType = AxisType.Secondary;
+
+            chart4.ChartAreas[strChartArea].AxisY2.Maximum = 150;
+
+            // locale specific percentage format with no decimals
+
+            chart4.ChartAreas[strChartArea].AxisY2.LabelStyle.Format = "P0";
+
+            // turn off the end point values of the primary X axis
+
+            chart4.ChartAreas[strChartArea].AxisX.LabelStyle.IsEndLabelVisible = false;
+
+            double percentage = 0.0;
+
+            foreach (DataPoint pt in chart4.Series[0].Points)
+
+            {
+
+                percentage += (pt.YValues[0] / total * 100.0);
+
+                destSeries.Points.Add(Math.Round(percentage, 2));
+
+            }            
+        }
+
+        /// <summary>
+        /// Reference: https://docs.microsoft.com/en-us/dotnet/api/system.web.ui.datavisualization.charting.stripline?redirectedfrom=MSDN&view=netframework-4.7.2
+        /// </summary>
+        private void AddHorizRepeatingStripLines()
+        {
+
+
+
+            // Instantiate max strip line  
+            StripLine stripLine1 = new StripLine();
+            stripLine1.StripWidth = 0;
+            stripLine1.BorderColor = Color.Red;
+            stripLine1.BorderWidth = 2;
+            stripLine1.IntervalOffset = 24;
+
+            //Instantiate min strip line
+            StripLine stripLine2 = new StripLine();
+            stripLine2.StripWidth = 0;
+            stripLine2.BorderColor = Color.Yellow;
+            stripLine2.BorderWidth = 2;
+            stripLine2.IntervalOffset = 2;
+
+            //Instaniate mean strip line
+            StripLine stripLine3 = new StripLine();
+            stripLine3.StripWidth = 0;
+            stripLine3.BorderColor = Color.Green;
+            stripLine3.BorderWidth = 2;
+            stripLine3.IntervalOffset = 12; 
+
+            // Add the strip line to the chart  
+            chart3.ChartAreas[0].AxisY.StripLines.Add(stripLine1);
+            chart3.ChartAreas[0].AxisY.StripLines.Add(stripLine2);
+            chart3.ChartAreas[0].AxisY.StripLines.Add(stripLine3);
+
+            //chart3.Series[1].MarkerColor = Color.Red;            
+            chart3.Series.Add("Maximun");
+            chart3.Series["Maximun"].Color = Color.Red;   
+            chart3.Series.Add("Minimun");
+            chart3.Series["Minimun"].Color = Color.Yellow;
+            chart3.Series.Add("Average");
+            chart3.Series["Average"].Color = Color.Green;
+
+
+            //chart4.ChartAreas[0].AxisY.StripLines.Add(stripLine1);
+            //chart4.ChartAreas[0].AxisY.StripLines.Add(stripLine2);
+            //chart4.ChartAreas[0].AxisY.StripLines.Add(stripLine3);
+        }
+
         public void PopulateChart()
         {
             //Creid: https://social.msdn.microsoft.com/Forums/en-US/34dc9031-af8b-444c-8eab-574dc4ad561e/how-to-bind-datagridview-columns-and-chart-control-in-c?forum=MSWinWebChart
-            
-            chart1.Series[0] = new Series();
+
+            chart1.Series[0].ChartType = SeriesChartType.Pie;
             chart1.Series[0].XValueMember = dgv1.Columns[0].DataPropertyName;
             chart1.Series[0].YValueMembers = dgv1.Columns[1].DataPropertyName;
             chart1.DataSource = dgv1.DataSource;
+            chart1.DataBind();
 
+            //"Databind" Study Reference: https://zh.coursera.org/lecture/dot-net-kaifa-jishu/shu-ju-bang-ding-gai-nian-ji-yu-fa-hAbQl
 
-            chart2.Series[0] = new Series();
+            chart2.Series[0].ChartType = SeriesChartType.Line;
             chart2.Series[0].XValueMember = dgv1.Columns[0].DataPropertyName;
             chart2.Series[0].YValueMembers = dgv1.Columns[1].DataPropertyName;
             chart2.DataSource = dgv1.DataSource;
+            chart2.DataBind();
+
+            //chart3.Series[0].YValuesPerPoint = dgv1.Columns[1]
+            chart3.Series[0].ChartType = SeriesChartType.Line;
+            chart3.Series[0].XValueMember = dgv1.Columns[0].DataPropertyName;
+            chart3.Series[0].YValueMembers = dgv1.Columns[1].DataPropertyName;
+            chart3.DataSource = dgv1.DataSource;
+            chart3.DataBind();
+
+            //int avr = chart3.Series[0].YValueMembers
+            chart4.Series[0].ChartType = SeriesChartType.Column;
+            chart4.Series[0].XValueMember = dgv1.Columns[0].DataPropertyName;
+            chart4.Series[0].YValueMembers = dgv1.Columns[1].DataPropertyName;
+            chart4.DataSource = dgv1.DataSource;
+            chart4.DataBind();
         }
 
         private void btnUpdate_Click(object sender, EventArgs e)
